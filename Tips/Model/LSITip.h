82cef23a5f36426995d1c4c3afa6a6d759ed6c9d
Pravin Patel <patelpra337@gmail.com>
2020-07-23 20:45:10 -0500

    Add prefix and new model

---
+++ Tips/Model/LSITip.h
@@ -0,0 +1,17 @@
+//
+//  LSITip.h
+//  Tips
+//
+//  Created by patelpra on 7/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITip : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tips/Model/LSITip.m
@@ -0,0 +1,13 @@
+//
+//  LSITip.m
+//  Tips
+//
+//  Created by patelpra on 7/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITip.h"
+
+@implementation LSITip
+
+@end
